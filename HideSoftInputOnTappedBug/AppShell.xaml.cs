dda743939a04ba865a5cd09b2c825cf4e50df6f6
kernshen <kernshen@gmail.com>
2024-04-06 22:00:51 +0800

    Add project files.

---
+++ HideSoftInputOnTappedBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HideSoftInputOnTappedBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
